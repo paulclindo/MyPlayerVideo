e1b553eb75a384957d104aabbcd215e6d61c36be
Paul Ccari <paulclindo@gmail.com>
2020-04-03 02:26:58 -0500

    fix fonts

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Baloo+Da+2:400,600,700&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
 
   html {
     box-sizing: border-box;
